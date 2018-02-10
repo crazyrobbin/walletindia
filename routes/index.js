2c2118a2153d3d7818f12e48a1308f9a5c6ccdcd
Robbin <crazyrobbin@gmail.com>
2018-02-10 20:17:07 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { page : 'index', title: '' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { page : 'about', title: 'About' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { page : 'contact', title: 'About' });
+});
+
+
+module.exports = router;
